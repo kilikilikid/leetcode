ddee04a7084c3de671f84d52a0a540111003b78f
Yunlong <yl@Yunlong-Dell.localdomain>
2022-06-23 15:54:37 +0100

    0200_NumberOfIslands UnionFind solution modified.

--- 0200_NumberOfIslands.cpp
+++ 0200_NumberOfIslands.cpp
@@ -158,12 +158,12 @@ int numIslands_unifind(std::vector<std::vector<char>>& grid) {
 	for (int r = 0; r < nr; ++r) {
 		for (int c = 0; c < nc; c++) {
 			if (grid[r][c] == '1') {
-				if (r - 1 >= 0 && grid[r - 1][c] == '1') 
-					uf.unite(r * nc + c, (r - 1) * nc + c);
+				// if (r - 1 >= 0 && grid[r - 1][c] == '1') 
+					// uf.unite(r * nc + c, (r - 1) * nc + c);
 				if (r + 1 < nr && grid[r + 1][c] == '1') 
 					uf.unite(r * nc + c, (r + 1) * nc + c);
-				if (c - 1 >= 0 && grid[r][c - 1] == '1') 
-					uf.unite(r * nc + c, r * nc + c - 1);
+				// if (c - 1 >= 0 && grid[r][c - 1] == '1') 
+					// uf.unite(r * nc + c, r * nc + c - 1);
 				if (c + 1 < nc && grid[r][c + 1] == '1') 
 					uf.unite(r * nc + c, r * nc + c + 1);
 			}
