6f0beefb74aee5cd4edf39cefd7ba9acc43848e2
yl <kidwong0227@gmail.com>
2022-06-24 23:09:18 +0100

    add 0133_CloneGraph solution

--- 0133_CloneGraph.cpp
+++ 0133_CloneGraph.cpp
@@ -20,11 +20,18 @@ Node* cloneGraph(Node* node);
 
 
 int main() {
-    Node* node;
+	unordered_map<Node*, Node*> visited;
+	Node* node;
     std::cout << cloneGraph(node) << std::endl;
     return 0;
 }
 
 Node* cloneGraph(Node* node) {
-	
+	if (node == nullptr) return nullptr;
+	if (visited.find(node) != visited.end()) return visited[node];
+	Node* cloneNode = new Node(node->val);
+	visited[node] = cloneNode;
+	for (auto& neighbor : node->neighbors)
+		cloneNode->neighbors.emplace_back(cloneGraph(neighbor));
+	return cloneNode;
 }
